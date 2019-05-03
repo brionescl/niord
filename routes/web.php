3a403cd1327483da4b71e9c834562f6174f69e4e
brionescl <brionescl@gmail.com>
2019-05-02 22:16:20 -0400

    menu

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
 
 Route::apiResource('expenses', 'ExpenseController');
 Route::apiResource('categories', 'CategoryController');
