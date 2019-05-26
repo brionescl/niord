1a4ccbf80230f115c80b79e8b78c681081f0040e
brionescl <brionescl@gmail.com>
2019-05-25 21:09:37 -0400

    aplicar margen superior

--- resources/js/components/BaseLoading.vue
+++ resources/js/components/BaseLoading.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container text-center">
+    <div class="container text-center mt-3">
         <div class="row">
             <div class="col">
                 <div class="spinner-border text-primary" role="status">
